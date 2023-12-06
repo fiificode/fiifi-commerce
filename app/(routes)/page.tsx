a848f77afd5289d9d8049f71a5bdc9c5ffd231c3
Franklin Mensah <fiificode@gmail.com>
2023-12-06 18:58:21 +0000

    feat:Added Billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <h1>Hello Commerce</h1>;
-}
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard/Billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const Home = async () => {
+  const billboard = await getBillboard("364171c0-3008-438a-b754-f11bc9058dcc");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
+};
+
+export default Home;
