ac798ec9c00833195e2c7fa57d619aee2804089c
Franklin Mensah <fiificode@gmail.com>
2023-12-06 22:01:35 +0000

    feat: Added featured products

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,15 +1,21 @@
 import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
 import Billboard from "@/components/billboard/Billboard";
+import ProductList from "@/components/productList/ProductList";
 import Container from "@/components/ui/container";
 
 export const revalidate = 0;
 
 const Home = async () => {
   const billboard = await getBillboard("364171c0-3008-438a-b754-f11bc9058dcc");
+  const products = await getProducts({ isFeatured: true });
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
       </div>
     </Container>
   );
---
+++ components/productCard/ProductCard.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { Product } from "@/types";
+import Image from "next/image";
+import { Button } from "../ui/button";
+import { ExpandIcon, ShoppingCart } from "lucide-react";
+import Currency from "../currency/Currency";
+
+interface ProductCardProps {
+  data: Product;
+}
+
+const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+  return (
+    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+      <div className="aspect-square rounded-xl bg-gray-100 relative">
+        <Image
+          src={data?.images?.[0].url}
+          alt="image"
+          priority
+          fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          className="aspect-square object-cover rounded-md"
+        />
+        <div className="opacity-0 group-hover:opacity-100 transition absolute w-full p-6 bottom-5">
+          <div className="flex gap-x-6 justify-center">
+            <Button
+              className="bg-white rounded-full"
+              size={"icon"}
+              onClick={() => {}}
+            >
+              <ExpandIcon size={20} className="text-gray-600" />
+            </Button>
+            <Button
+              className="bg-white rounded-full"
+              size={"icon"}
+              onClick={() => {}}
+            >
+              <ShoppingCart size={20} className="text-gray-600" />
+            </Button>
+          </div>
+        </div>
+      </div>
+      {/**Description */}
+      <div>
+        <p className="font-semibold text-lg">{data?.name}</p>
+        <p className="tex-sm text-gray-500">{data?.category?.name}</p>
+      </div>
+      {/**Price */}
+      <div className="flex items-center justify-between">
+        <Currency value={data?.price} />
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
