eff17dca5115691918c58bfc7cd30e3321518471
Franklin Mensah <fiificode@gmail.com>
2023-12-07 17:52:49 +0000

    feat:Added mobile filters

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -9,6 +9,7 @@ import Filter from "./components/filter";
 import NoResults from "@/components/ui/no-results";
 import ProductCard from "@/components/productCard/ProductCard";
 import { it } from "node:test";
+import MobileFilter from "./components/MobileFilter";
 
 export const revalidate = 0;
 
@@ -41,7 +42,7 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
         <Billboard data={category?.billboard} />
         <div className="px-4 sm:px-6 lg:px-8 pb-24">
           <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
-            {/**Add mobile filters */}
+            <MobileFilter sizes={sizes} colors={colors} />
             <div className="hidden lg:block">
               <Filter valueKey={"sizeId"} name="Sizes" data={sizes} />
               <Filter valueKey={"colorId"} name="Colors" data={colors} />
